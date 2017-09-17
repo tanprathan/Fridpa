197a72187427235b082d14833d3dc8a12ac6608b
Prathan Phongthiproek <tanprathan@users.noreply.github.com>
2017-09-17 17:46:54 +0700

    Add files via upload

---
+++ fridpa.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# An automated wrapper script for unpacking, patching, signing and deploying
+# apps on non-jailbroken device. Once the process is completed, the apps will
+# launch in debugging mode with lldb attached and ready for hooking using Frida
+#
+# Developed by Prathan Phongthiproek 
+
+export PATH=$PATH:/usr/libexec
+
+function patch()
+{
+# Clear Payload folder
+rm -rf Payload*
+
+# Unpacking iOS package:
+APPPACKAGE="$(find *.ipa)"
+
+echo "" && echo -e "***** Listing Signing Identity *****"
+security find-identity -p codesigning -v
+echo "" && echo -n "Enter your Identity and press [ENTER]: "
+read SID
+echo "" && echo -e "***** Unpacking iOS package: $APPPACKAGE *****"
+unzip ${APPPACKAGE} > /dev/null
+
+# Identifying Application Name:
+APPNAME="$(ls Payload)"
+
+# Copying Frida to Application folder and Inserting load command
+cp FridaGadget.dylib "Payload/${APPNAME}/"
+echo "" && echo -e "***** Inserting load command into Binary *****"
+APPBINARY=${APPNAME%.*}
+./optool install -c load -p "@executable_path/FridaGadget.dylib" -t "Payload/${APPNAME}/${APPBINARY}" > /dev/null
+
+# Obtaining entitlements and Bundle ID:
+security cms -D -i embedded.mobileprovision > profile.plist
+PlistBuddy -x -c 'Print :Entitlements' profile.plist > entitlements.plist
+
+ENT=$(egrep -a -A 2 application-identifier embedded.mobileprovision | grep string | sed -e 's/<string>//' -e 's/<\/string>//' -e 's/ //' | tr -d '\t')
+BUNDLEID=${ENT#*.}
+
+# Signing the Application package:
+cp embedded.mobileprovision "Payload/${APPNAME}/embedded.mobileprovision"
+
+PlistBuddy -c "Set :CFBundleIdentifier ${BUNDLEID}" "Payload/${APPNAME}/Info.plist"
+
+rm -rf Payload/${APPNAME}/_CodeSignature
+echo "" && echo -e "***** Re-signing Binary *****"
+codesign --force --sign ${SID} "Payload/${APPNAME}/FridaGadget.dylib"
+codesign --force --sign ${SID} --entitlements entitlements.plist "Payload/${APPNAME}/${APPBINARY}"
+
+# Clear entitlements file
+rm entitlements.plist
+rm profile.plist
+
+# Deploying application with debuggable mode:
+ios-deploy --bundle "Payload/${APPNAME}/"
+echo "" && echo -n "Trust Developer profile on Settings and press [ENTER]:"
+read OK
+echo "" && echo -e "***** Deploying Application with Frida Server *****"
+ios-deploy --debug --bundle "Payload/${APPNAME}/"
+exit 1
+}
+
+function deploy()
+{
+APPNAME="$(ls Payload)"
+   ios-deploy --debug --bundle "Payload/${APPNAME}/"
+   exit 1
+}
+
+
+# Providing option for Fridpa
+cat welcome.txt
+echo "" && echo -n "Enter your Option for Fridpa and press [ENTER]: "
+read option
+
+case ${option} in
+   1) patch
+      ;;
+   2) deploy
+      ;;
+   *)
+      echo ""
+      echo "++++++++++++++++++++++++++++++"
+      echo "+                            +"
+      echo "+  Noob Spotted, Go away !!  +"
+      echo "+                            +"
+      echo "++++++++++++++++++++++++++++++"
+      echo ""
+      exit 1 # Command to come out of the program with status 1
+      ;;
+esac
+
+sleep 10
